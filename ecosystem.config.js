935f5ea2ae69da2df95e9a6d35e9951d00e82e74
error171 <dqngthng@gmail.com>
2022-09-15 14:49:28 +0700

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,11 @@ module.exports = {
     watch: ["server", "client"],
     watch_delay: 1000,
     ignore_watch : ["node_modules", "server/uploads"],
-  }]
+  }],
+  env: {
+    NODE_ENV: "development",
+  },
+  env_production: {
+    NODE_ENV: "production",
+  }
 }
